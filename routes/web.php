e96bd8c6d73a29f643131f8a7a74b17b27902489
unknown <tripodesystemas@gmail.com>
2019-05-21 20:00:26 -0600

    routes/web.php

--- routes/web.php
+++ routes/web.php
@@ -30,6 +30,12 @@ Route::resource('facturas','FacturaController');
 
 Route::resource('clientes','ClienteController');
 
+Route::resource('productos','ProductoController');
+
+Route::get('/busqueda','BusquedaController@index');
+
+Route::post('/busqueda/fetch','BusquedaController@fetch')->name('busqueda.fetch');
+
 // Route::get('clientes',function(){
 // 	$clientes = Cliente::get();
 // 	return $clientes;
